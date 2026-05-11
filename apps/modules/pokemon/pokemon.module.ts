fc36f8ffa3d26ce759cceca5e4202cab836ecf2f
Mikita Pilets <mykytapilec@gmail.com>
2026-05-12 00:12:21 +0200

    chore: clean up monorepo structure

--- apps/modules/pokemon/pokemon.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PokemonController } from './pokemon.controller';
-import { PokemonService } from './pokemon.service';
-
-@Module({
-  controllers: [PokemonController],
-  providers: [PokemonService]
-})
-export class PokemonModule {}
