bb569886d84213bed77ec9c7c83c82a2eda4ab72
Mikita Pilets <mykytapilec@gmail.com>
2026-05-11 16:47:27 +0200

    feat: implement collections module with validation and MongoDB persistence

---
+++ apps/backend/src/modules/collections/collections.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CollectionsService } from './collections.service';
+import { CreateCollectionDto } from './dto/create-collection.dto';
+
+@Controller('collections')
+export class CollectionsController {
+  constructor(private readonly service: CollectionsService) {}
+
+  @Post()
+  create(@Body() dto: CreateCollectionDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.delete(id);
+  }
+}
